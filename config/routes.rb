cd45ad1c608c339b5c42f37347a59213b6537a51
ARAKI Yasuhiro <ar@debian.org>
2011-08-09 23:18:56 +0900

    change for /hosts/new

--- config/routes.rb
+++ config/routes.rb
@@ -56,6 +56,7 @@ CnameQRV4::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
 
+  match 'hosts/new', :to => 'hosts#new'
   match 'hosts/:_id', :to => 'hosts#show', :via => :get
   match 'hosts/:_id', :to => 'hosts#update', :via => :put
   match 'hosts/:_id', :to => 'hosts#destroy', :via => :delete
