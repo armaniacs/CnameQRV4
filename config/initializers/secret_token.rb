9e27f6124a41f28481fba6be26b19bd055618fe7
ARAKI Yasuhiro <ar@debian.org>
2013-01-06 23:07:08 +0900

    delete secret_token.rb and adding .gitignore

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-CnameQRV4::Application.config.secret_token = 'cfdf6e5e25278a9f5b039e42f5674aded7609c8cfe9c705b86dc8402406a3350319e37254d943dc4e8cb637d2fec4230ba97dee17004e603291464c731e9a384'
